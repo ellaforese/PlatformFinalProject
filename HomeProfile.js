ccb052fc723dfba428fbd994cdc064895073850a
YinWahYip <yyip@villanova.edu>
2023-04-30 18:35:14 -0400

    fixed most of the code?? Added and fixed files

--- HomeProfile.js
+++ HomeProfile.js
@@ -1,26 +1,185 @@
+// import { StyleSheet, Text, View, Image, ScrollView, Button, TextInput, TouchableOpacity, FlatList } from 'react-native';
+// import { useState } from 'react';
+// import { PhotoTaker } from './PhotoTaker'; // import the PhotoTaker component
+// import * as ImagePicker from 'expo-image-picker';
+
+// export default function HomeProfile({ navigation }) {
+//   const [songs, setSongs] = useState([]);
+//   const [imageUri, setImageUri] = useState(null);
+
+//   const addSong = (song) => {
+//     setSongs([...songs, song]);
+//   };
+
+//   const renderItem = ({ item }) => (
+//     <Text style={styles.listItem}>{item}</Text>
+//   );
+
+//   const pickImage = async () => {
+//     let result = await ImagePicker.launchImageLibraryAsync({
+//       mediaTypes: ImagePicker.MediaTypeOptions.Images,
+//       allowsEditing: true,
+//       aspect: [4, 3],
+//       quality: 1,
+//     });
+
+//     if (!result.cancelled) {
+//       setImageUri(result.uri);
+//     }
+//   };
+
+//   return (
+//     <View style={styles.container}>
+//       <ScrollView>
+//         <TextInput
+//           style={[styles.nameInput, { textAlign: 'center' }]}
+//           placeholder="Your name"
+//           maxLength={50}
+//         />
+
+//         <TouchableOpacity onPress={() => navigation.navigate('Profile Photo')}>
+//           <Text style={[styles.title, { textAlign: 'center' }]}>Take a Profile Picture</Text>
+//         </TouchableOpacity>
+
+//         <TouchableOpacity onPress={pickImage}>
+//           <Text style={[styles.title, { textAlign: 'center' }]}>Upload Picture!</Text>
+//         </TouchableOpacity>
+
+//         {imageUri && <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />}
+
+//         <TouchableOpacity onPress={() => navigation.navigate('Find Songs')}>
+//           <Text style={[styles.title, { textAlign: 'center' }]}>Find Songs</Text>
+//         </TouchableOpacity>
+
+//         <Text style={[styles.words, { textAlign: 'left' }]}> Songs You Liked:</Text>
+//         <View style={styles.container}>
+//           <View style={styles.container}>
+//             <TextInput
+//               style={[styles.listInput, { textAlign: 'left' }]}
+//               placeholder="Add a song"
+//               maxLength={50}
+//               onSubmitEditing={(event) => addSong(event.nativeEvent.text)}
+//             />
+//             <FlatList
+//               data={songs}
+//               renderItem={renderItem}
+//               keyExtractor={(item, index) => index.toString()}
+//             />
+//           </View>
+//         </View>
+//       </ScrollView>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     paddingLeft: 5,
+//     paddingRight: 5,
+//     paddingBottom: 50,
+//     backgroundColor: 'lightblue'
+//   },
+//   title: {
+//     fontSize: 40,
+//     fontFamily: 'Georgia-Bold',
+//   },
+//   words:
+//   {
+//     fontSize: 20,
+//     fontFamily: 'Georgia-Bold',
+//   },
+//   nameInput:
+//   {
+//     fontSize: 40,
+//     fontFamily: 'Georgia-Bold',
+//   },
+//   listInput:
+//   {
+//     fontSize: 20,
+//     fontFamily: 'Georgia-Bold',
+//   },
+// });
+
+
+
+
+
 import { StyleSheet, Text, View, Image, ScrollView, Button, TextInput, TouchableOpacity, FlatList } from 'react-native';
 import { useState } from 'react';
 import { PhotoTaker } from './PhotoTaker'; // import the PhotoTaker component
+import * as ImagePicker from 'expo-image-picker';
+
+// import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
 
 export default function HomeProfile({ navigation }) {
   const [songs, setSongs] = useState([]);
+  const [artists, setArtists] = useState([]);
+  const [showPlaceholder, setShowPlaceholder] = useState(true);
+  const [imageUri, setImageUri] = useState(null);
+
 
   const addSong = (song) => {
     setSongs([...songs, song]);
+    //how to get it so that upon pressing submit the "add a song" placeholdrer will come back
 
   };
 
-  const renderItem = ({ item }) => (
+  const addArtist = (artist) => {
+    setArtists([...artists, artist]);
+
+  };
+
+  const renderSongItem = ({ item }) => (
+    <Text style={styles.listItem}>{item}</Text>
+
+  );
+
+  const renderArtistItem = ({ item }) => (
     <Text style={styles.listItem}>{item}</Text>
   );
 
+  // const songsList = songs.map((song, index) => renderSongItem(song, index));
+  // const artistsList = artists.map((artist, index) => renderArtistItem(artist, index));
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      setImageUri(result.uri);
+    }
+  };
+
 
   return (
+
     <View style={styles.container}>
       <ScrollView>
+
+        <View style={styles.headphoneContainer}>
+          <Image source={require('./headphones.jpeg')}
+            style={{ width: 98, height: 90 }}
+          />
+          <Image source={require('./headphones.jpeg')}
+            style={{ width: 98, height: 90 }}
+          />
+          <Image source={require('./headphones.jpeg')}
+            style={{ width: 98, height: 90 }}
+          />
+          <Image source={require('./headphones.jpeg')}
+            style={{ width: 98, height: 90 }}
+          />
+        </View>
+
         <TextInput
-          style={[styles.nameInput, { textAlign: 'center' }]}
-          placeholder="Your name"
+          style={[styles.title, { textAlign: 'center' }]}
+          placeholder="Your Name"
           maxLength={50}
         //add wrap around capability here
         />
@@ -29,32 +188,72 @@ export default function HomeProfile({ navigation }) {
           <Text style={[styles.title, { textAlign: 'center' }]}>Take a Profile Picture</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity onPress={() => navigation.navigate('Upload Picture')}>
+        <TouchableOpacity onPress={pickImage}>
           <Text style={[styles.title, { textAlign: 'center' }]}>Upload Picture!</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity onPress={() => navigation.navigate('Find Songs')}>
-          <Text style={[styles.title, { textAlign: 'center' }]}>Find Songs</Text>
+        {imageUri && <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />}
+
+        <TouchableOpacity onPress={() => navigation.navigate('Search')}>
+          <Text style={[styles.title, { textAlign: 'center' }]}>Search</Text>
         </TouchableOpacity>
 
-        <Text style={[styles.words, { textAlign: 'left' }]}> Songs You Liked:</Text>
-        <View style={styles.container}>
-          <View style={styles.container}>
+        <View style={styles.listContainer1}>
+          <Text style={[styles.words, { textAlign: 'left' }]}> Songs You Liked:</Text>
+          <View style={styles.text}>
             <TextInput
-              style={[styles.listInput, { textAlign: 'left' }]}
+              style={[styles.words, { textAlign: 'left' }]}
               placeholder="Add a song"
               maxLength={50}
               onSubmitEditing={(event) => addSong(event.nativeEvent.text)}
+            // setShowPlaceholder(false);
             />
-            <FlatList
-              data={songs}
-              renderItem={renderItem}
-              keyExtractor={(item, index) => index.toString()}
-            />
+            <View>
+              <FlatList
+                data={songs}
+                renderItem={renderSongItem}
+                keyExtractor={(item, index) => index.toString()}
+              />
+
+              {/* <ScrollView>
+              {songsList}
+            </ScrollView> */}
+
+            </View>
+
           </View>
         </View>
+
+        <View style={styles.listContainer2}>
+          <Text style={[styles.words, { textAlign: 'left' }]}> Artists You Liked:</Text>
+          <View style={styles.text}>
+            <TextInput
+              style={[styles.words, { textAlign: 'left' }]}
+              placeholder="Add an artist"
+              maxLength={50}
+              onSubmitEditing={(event) => addArtist(event.nativeEvent.text)}
+            // setShowPlaceholder(false);
+            />
+            {/* <ScrollView>
+              {artistsList}
+            </ScrollView> */}
+
+            <View>
+              <FlatList
+                data={artists}
+                renderItem={renderArtistItem}
+                keyExtractor={(item, index) => index.toString()}
+              />
+            </View>
+          </View>
+
+        </View>
       </ScrollView>
+
     </View>
+
+
+
 
   );
 
@@ -62,10 +261,15 @@ export default function HomeProfile({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingLeft: 5,
-    paddingRight: 5,
+    //paddingLeft: 5,
+    // paddingRight: 5,
+
     paddingBottom: 50,
-    backgroundColor: 'lightblue'
+    backgroundColor: 'coral'
+  },
+  text: {
+    paddingLeft: 10,
+
   },
   title: {
     fontSize: 40,
@@ -76,18 +280,27 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontFamily: 'Georgia-Bold',
   },
-  nameInput:
-  {
-    fontSize: 40,
-    fontFamily: 'Georgia-Bold',
 
+
+  headphoneContainer: {
+    flexDirection: 'row',
   },
-  listInput:
+
+  listContainer1:
   {
-    fontSize: 20,
-    fontFamily: 'Georgia-Bold',
+    flexDirection: 'column',
+    flex: 1,
+    borderTopWidth: 5,
+    borderBottomWidth: 2.5,
+  },
+
+  listContainer2:
+  {
+    flexDirection: 'column',
+    flex: 1,
+    borderBottomWidth: 5,
+    borderTopWidth: 2.5,
   },
 
 });
-
 
--- OpeningScreen.js
+++ OpeningScreen.js
@@ -1,42 +1,102 @@
-import { StyleSheet, Text, View, ScrollView, Image, TouchableOpacity} from 'react-native';
+// import { StyleSheet, Text, View, ScrollView, Image, TouchableOpacity} from 'react-native';
 
 
-export default function OpeningScreen({navigation}) {
-    return (
+// export default function OpeningScreen({navigation}) {
+//     return (
+//     <View style={styles.container}>
+//         {/* <Image
+//         source={require('/Users/ellaforese/Desktop/CSC2053/PlatformFinalProjectMP3/mp3Player/music-notes-color.jpeg')
+//         }
+//         style={{ width: 400, height: 300 }}
+//         >
+//         </Image> */}
+
+//         <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+//         <Text style={[styles.title, {textAlign: 'center'}]}>Begin Listening</Text>
+//       </TouchableOpacity>
+
+//       {/* <Image
+//         source={require('/Users/ellaforese/Desktop/CSC2053/PlatformFinalProjectMP3/mp3Player/music-notes-color.jpeg')
+//         }
+//         style={{ width: 400, height: 300 }}
+//         >
+//         </Image> */}
+
+//     </View>
+//     );
+
+// }
+
+// const styles = StyleSheet.create({
+//     container:{
+//       flex: 1,
+//       paddingLeft: 5,
+//       paddingRight: 5,
+//       paddingBottom: 50,
+//       backgroundColor: 'lightblue'
+//     },
+//     title: {
+//       fontSize: 40,
+//       fontFamily: 'Georgia-Bold',
+//     },
+// });
+
+
+import { StyleSheet, Text, View, ScrollView, Image, TouchableOpacity, ImageBackground } from 'react-native';
+
+
+export default function OpeningScreen({ navigation }) {
+  return (
     <View style={styles.container}>
-        {/* <Image
-        source={require('/Users/ellaforese/Desktop/CSC2053/PlatformFinalProjectMP3/mp3Player/music-notes-color.jpeg')
-        }
-        style={{ width: 400, height: 300 }}
+      {/* <Image
+        source={require('./music1.png')}
+        style={{ width: 420, height: 200 }}
         >
         </Image> */}
 
-        <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+
+      <ImageBackground
+        source={require('./music3.jpeg')}
+        style={{ width: 400, height: 800 }}
+      >
+        <TouchableOpacity onPress={() => navigation.navigate('Home')} style={styles.words}>
+          <Text style={styles.text}>Click anywhere to get started</Text>
+        </TouchableOpacity>
+
+      </ImageBackground>
+
+
+
+      {/* <TouchableOpacity onPress={() => navigation.navigate('Home')}>
         <Text style={[styles.title, {textAlign: 'center'}]}>Begin Listening</Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
 
       {/* <Image
-        source={require('/Users/ellaforese/Desktop/CSC2053/PlatformFinalProjectMP3/mp3Player/music-notes-color.jpeg')
-        }
-        style={{ width: 400, height: 300 }}
+        source={require('./music2.jpeg')}
+        style={{ width: 400, height: 450 }}
         >
         </Image> */}
 
     </View>
-    );
+  );
 
 }
 
 const styles = StyleSheet.create({
-    container:{
-      flex: 1,
-      paddingLeft: 5,
-      paddingRight: 5,
-      paddingBottom: 50,
-      backgroundColor: 'lightblue'
-    },
-    title: {
-      fontSize: 40,
-      fontFamily: 'Georgia-Bold',
-    },
+  container: {
+    flex: 1,
+    // paddingLeft: 0,
+    // paddingRight: 5,
+
+    // backgroundColor: 'lightblue'
+  },
+  words: {
+    fontSize: 80,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 1300,
+
+  },
+
+
 });
